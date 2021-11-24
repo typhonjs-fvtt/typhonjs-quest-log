d76ec151c0829236e55ae380aa5e814d41dfa9af
typhonrt <support@typhonjs.io>
2021-11-24 14:40:00 -0800

    Switched to terserConfig from @typhonjs-fvtt/runtime.

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,4 +1,5 @@
 import alias               from '@rollup/plugin-alias';
+import { babel }           from '@rollup/plugin-babel';
 import commonjs            from '@rollup/plugin-commonjs';
 import postcss             from 'rollup-plugin-postcss';       // Process Sass / CSS w/ PostCSS
 import resolve             from '@rollup/plugin-node-resolve'; // This resolves NPM modules from node_modules.
@@ -7,17 +8,9 @@ import svelte              from 'rollup-plugin-svelte';
 import { terser }          from 'rollup-plugin-terser';        // Terser is used for minification / mangling
 import {
    postcssConfig,
+   terserConfig,
    typhonjsRuntime }       from '@typhonjs-fvtt/runtime/rollup';
 import virtual             from '@rollup/plugin-virtual';
-
-// `typhonjs-quest-log.css`
-// This plugin is for importing existing sourcemaps from NPM modules. Include it for
-// any external imported source code that has sourcemaps available.
-// import sourcemaps       from 'rollup-plugin-sourcemaps';
-
-// Import config files for Terser and Postcss; refer to respective documentation for more information.
-// We are using `require` here in order to be compliant w/ `fvttdev` for testing purposes.
-import terserConfig  from './terser.config.js';
 
 const s_COMPRESS = true;
 const s_SOURCEMAPS = true;
@@ -39,7 +32,7 @@ export default () =>
 {
    // Defines potential output plugins to use conditionally if the .env file indicates the bundles should be
    // minified / mangled.
-   const outputPlugins = s_COMPRESS ? [terser(terserConfig)] : [];
+   const outputPlugins = s_COMPRESS ? [terser(terserConfig())] : [];
 
    // Defines whether source maps are generated / loaded from the .env file.
    const sourcemap = s_SOURCEMAPS;
@@ -79,7 +72,13 @@ export default () =>
             }),
             commonjs(),
             // sourcemaps()
-            s_TYPHONJS_MODULE_LIB && typhonjsRuntime()
+            s_TYPHONJS_MODULE_LIB && typhonjsRuntime(),
+            babel({
+               babelHelpers: 'bundled',
+               presets: [
+                  ['@babel/preset-env', { bugfixes: true, shippedProposals: true, targets: { esmodules: true } }]
+               ]
+            })
          ]
       },
       {  // A 2nd virtual bundle to process TinyMCE CSS separately.

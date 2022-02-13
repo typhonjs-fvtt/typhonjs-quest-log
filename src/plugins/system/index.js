77abf0c899814fa39ae88864da3894c5fb2543f4
typhonrt <support@typhonjs.io>
2022-02-12 20:46:32 -0800

    remove unused plugin

--- src/plugins/system/index.js
+++ src/plugins/system/index.js
@@ -4,7 +4,6 @@ export { default as SettingsControl }  from './settings/SettingsControl.js';
 export * as QuestAPIModule             from './public/QuestAPI.js';
 
 export { default as QuestDB }          from './database/QuestDB.js';
-export { default as FoundryUIManager } from './ui/FoundryUIManager.js';
 export { default as ViewManager }      from './ui/ViewManager.js';
 export { default as Socket }           from './net/Socket.js';
 export { default as DBMigration }      from './database/migration/DBMigration.js';

ea22e35fedfa2d417041ef0dd5526dbe373efceb
typhonrt <support@typhonjs.io>
2022-02-17 10:04:03 -0800

    moved to new Position implementation

--- src/view/tracker2/QuestTrackerApp.js
+++ src/view/tracker2/QuestTrackerApp.js
@@ -23,13 +23,6 @@ const s_DEFAULT_POSITION = { top: 80, width: s_DEFAULT_WIDTH };
 export default class QuestTrackerApp extends SvelteApplication
 {
    /**
-    * Stores CSS attributes for min / max width and height.
-    *
-    * @type {object}
-    */
-   #appExtents;
-
-   /**
     * @inheritDoc
     * @see https://foundryvtt.com/api/Application.html
     */
@@ -86,13 +79,6 @@ export default class QuestTrackerApp extends SvelteApplication
    }
 
    /**
-    * Gets the minimum width of this Application.
-    *
-    * @returns {number} Minimum width.
-    */
-   get minWidth() { return this.#appExtents.minWidth || 275; }
-
-   /**
     * Override default Application `bringToTop` to stop adjustment of z-index and set height to auto if not resizable.
     *
     * @override
@@ -101,7 +87,7 @@ export default class QuestTrackerApp extends SvelteApplication
     */
    bringToTop()
    {
-      if (!this.reactive.resizable) { this.elementTarget.style.height = 'auto'; }
+      if (!this.reactive.resizable) { this.position.height = 'auto'; }
    }
 
    /**
@@ -144,11 +130,6 @@ export default class QuestTrackerApp extends SvelteApplication
     */
    #handleSettingWindowResize(resizable)
    {
-      // Early out if there is no root element; resize setting can be set when there is no quest tracker rendered.
-      if (this.elementTarget === null || this.elementTarget === void 0) { return; }
-
-      const elementTarget = this.elementTarget;
-
       if (resizable)
       {
          // When resizable is set to true set the current position to the stored position. This allows the position
@@ -162,21 +143,13 @@ export default class QuestTrackerApp extends SvelteApplication
             console.log(`TyphonJSQuestLog - QuestTracker - #handleSettingWindowResize - error: ${err.message}`);
          }
 
-         elementTarget.style.minHeight = this.#appExtents.minHeight;
-
-         // Set height to null. This will cause setPosition to set the height style.
-         elementTarget.style.height = null;
-
-         this.setPosition(this.position);
+         // Set height to null. This will cause height to be set to current offset extent.
+         this.position.height = null;
       }
       else
       {
-         const elemWindowHeader = elementTarget.querySelector('#quest-tracker .window-header');
-
-         elementTarget.style.minHeight = elemWindowHeader.scrollHeight;
-
          // Set height to auto. This will cause QuestTrackerShell to save the new position.
-         elementTarget.style.height = 'auto';
+         this.position.height = 'auto';
       }
 
       this.reactive.resizable = resizable;
@@ -185,28 +158,10 @@ export default class QuestTrackerApp extends SvelteApplication
    /**
     * Defines all {@link JQuery} control callbacks with event listeners for click, drag, drop via various CSS selectors.
     *
-    * @param {HTMLElement}  element - The jQuery instance for the window content of this Application.
-    *
     * @see https://foundryvtt.com/api/FormApplication.html#activateListeners
     */
-   onSvelteMount({ elementTarget })
+   onSvelteMount()
    {
-      const styles = globalThis.getComputedStyle(elementTarget);
-
-      /**
-       * Stores the app / window extents from styles.
-       *
-       * @type {{minHeight: number, maxHeight: number, minWidth: number, maxWidth: number}}
-       *
-       * @private
-       */
-      this.#appExtents = {
-         minWidth: parseInt(styles.minWidth),
-         maxWidth: parseInt(styles.maxWidth),
-         minHeight: parseInt(styles.minHeight),
-         maxHeight: parseInt(styles.maxHeight)
-      };
-
       this.#handleSettingWindowResize(game.settings.get(constants.moduleName, settings.questTrackerResizable));
 
       PositionValidator.updateTracker();
